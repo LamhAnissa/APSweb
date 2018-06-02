d8286e00a0fe4c1122f564a4ca400d6c54db6501
LamhAnissa <32563083+LamhAnissa@users.noreply.github.com>
2018-06-02 23:13:20 +0200

    up

--- application/views/membre/monprofiladmin.php
+++ application/views/membre/monprofiladmin.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
