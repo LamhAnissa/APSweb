61873db85a36c933d3a63d5fb0b89ecc28f558a9
LamhAnissa <32563083+LamhAnissa@users.noreply.github.com>
2018-06-03 12:36:52 +0200

    last

---
+++ application/views/client/ficheClient.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<link rel="stylesheet" h_client="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>APS WEB / Mon Profil</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link h_client="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link h_client="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link h_client="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link h_client="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link h_client="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" h_client="index.html">APS Serv</a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" h_client="#">
+                        <i class="fa fa-envelope fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-messages">
+                        <li>
+                            <a h_client="#">
+                                
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a h_client="#">
+                               
+                            </a>
+                        </li>
+                       
+                        <li class="divider"></li>
+                  
+                    </ul>
+                    <!-- /.dropdown-messages -->
+                </li>
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" h_client="#">
+                        <i class="fa fa-tasks fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-tasks">
+                        <li>
+                            
+                        </li>
+
+                            <a class="text-center" h_client="#">
+                                <strong>See All Tasks</strong>
+                                <i class="fa fa-angle-right"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-tasks -->
+                </li>
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" h_client="#">
+                        <i class="fa fa-bell fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-alerts">
+                       
+                        <li class="divider"></li>
+                        <li>
+                            <a class="text-center" h_client="#">
+                             
+                                <i class="fa fa-angle-right"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-alerts -->
+                </li>
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" h_client="#">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a h_client="#"><i class="fa fa-user fa-fw"></i> Mon profils</a>
+                        </li>
+                        <li><a h_client="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a h_client="login.html"><i class="fa fa-sign-out fa-fw"></i> Se déconnecter</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                          <li>
+                            <a h_client="<?php echo site_url("client/monprofil")?>" ><i class="fa fa-table fa-fw"></i> Mon profil</a>
+                        </li>
+                        <li>
+                            <a h_client="<?php echo site_url("Lier/myContacts")?>" ><i class="fa fa-dashboard fa-fw"></i> Mes contacts</a>
+                        </li>
+
+                        <li>
+                            <a h_client="<?php echo site_url("#")?>" ><i class="fa fa-wrench fa-fw"></i> APS Service<span class="fa arrow"></span></a>
+                           
+                        </li>
+                        
+                        
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+
+          
+                            <div class="box-content">
+                                
+                                <div class="form-group">
+
+
+     <h2 style= "text-align:center">Fiche liaison de <?php echo $client['nom_client]'></h2>
+  <br> </br>            
+
+<div class="box-content no-padding">
+                <div class="row-fluid centered">
+                    <div class="col-sm-12"><h4> Nom : <?php echo $client['nom_client] ?></h4></div>
+                    <div class="col-sm-12"><h4> Prénom : <?php echo $client['prenom_client] ?></h4></div>
+                    <div class="col-sm-12"><h4> Rue : <?php echo $client[0]->rue_client; ?></h4></div>
+                    <div class="col-sm-12"><h4> Ville : <?php echo $client['ville_client]?></h4></div>
+                    <div class="col-sm-12"><h4> Code Postal: <?php echo $client['CP_client'] ?></h4></div>
+                    <div class="col-sm-12"><h4> Date de naissance : <?php echo $client['ddn_client] ?></h4></div>
+                    <div class="col-sm-12"><h4> Sous tutelle : <?php echo $client['sousTutelle]' ?></h4></div>
+                    <div class="col-sm-12"><h4> Perte Autonomie : <?php echo $client['perteAutonomie']></h4></div>                  
+                    <div class="col-sm-12"><h4> Perte Mémoire : <?php echo $client['perteMemoire']></h4></div>                  
+                    <div class="col-sm-12"><h4> Allergie : <?php echo $client[allergie] ?></h4></div>
+                    <div class="col-sm-12"><h4> Logement : <?php echo $client[logement]?></h4></div>    <div class="col-sm-12"><h4> Autres informations :<textarea ><?php echo $client[info_sup]?></textarea>
+                    <div class="clearfix"></div>
+                </div>
+<br> </br> 
+       
+
+
+
+
+
+             <div id="footer">
+      <div class="container ">
+        <p class="footer-block"  style="text-align:center;" style="color:Grey"><strong>   Projet Web - IG3 Polytech - LAMHAOUAR  &copy;</strong></p>  
+      </div>
+    </div>
+</body>
+</html>
